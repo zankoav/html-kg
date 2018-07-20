8624ef0116b75ba98bb8cbd33c2a6183ed7fd593
zankoav <zankoav@mail.ru>
2018-07-20 20:36:08 +0300

    update

---
+++ assets/bem/our-house/our-house.js
@@ -0,0 +1,1 @@
+import './our-house.scss';
--- assets/templates/houses.js
+++ assets/templates/houses.js
@@ -1,9 +1,7 @@
 import "./../bem/_base.scss";
 import "../bem/header-second/header-second";
 import './../bem/header-title/header-title';
-
-
 import './../bem/footer-top/footer-top';
 import './../bem/footer-bottom/footer-bottom';
-import './../bem/our-houses/our-houses';
+import './../bem/our-house/our-house';
 
